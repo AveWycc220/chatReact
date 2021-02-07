42e0f4236471e6f5f440d979ed808449e3f64a0c
AveWycc220 <mik16748@gmail.com>
2021-02-07 17:14:34 +0300

    feat (components): add animation for loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import GSAP from 'gsap'
 import './scss/loading.scss'
 
 export default class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    props.store.subscribe(() => {
+      const loadingElement = document.querySelector('.loading')
+      if (loadingElement) {
+        GSAP.fromTo('.loading',{opacity: 1 },{opacity: 0, duration: 0.15})
+      }
+    })
+  }
+
+  componentDidMount() {
+    GSAP.from('.loading', {opacity: 0, duration: 0.25})
+  }
+
   render() {
     return <div className="loading" />
   }
