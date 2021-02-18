1bcb18dea4217d133c5edd85f4d0f40a5d781ca8
AveWycc220 <mik16748@gmail.com>
2021-02-18 15:31:11 +0300

    feat (App): add error component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import './App.scss'
 import Loading from './components/Loading'
 import Form from './components/LoginPage/Form'
+import Error from './components/Error'
 
 export default function App(props) {
   const [connectionOpened, setConnectionOpened] = useState(props.store.getState().connectionOpened)
   const [isAuthorized, setIsAuthorized] = useState(props.store.getState().auth)
+  const [errorServer, setErrorServer] = useState(props.store.getState().errorServer)
   const authTimeoutDuration = 0.2
 
   const unsubscribe = props.store.subscribe(() => {
@@ -17,6 +19,7 @@ export default function App(props) {
       setIsAuthorized(props.store.getState().auth)
       clearTimeout(authTimeout)
     }, authTimeoutDuration * 1000)
+    setErrorServer(props.store.getState().errorServer)
   })
 
   useEffect(() => {
@@ -30,8 +33,8 @@ export default function App(props) {
           {isAuthorized ?  <p>MainPage</p> : <Form store={props.store} authTimeoutDuration={authTimeoutDuration} /> }
         </div>
       ) : (
-        <Loading store={props.store}/>
+        errorServer ? <Error errorInfo={props.store.getState().errorServer}/> : <Loading store={props.store}/>
       )}
     </>
-  );
+  )
 }
